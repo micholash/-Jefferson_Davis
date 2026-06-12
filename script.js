a8a975b1940349285fd9f7a099395074827e75d1
micholash <24422@daehyun.hs.kr>
2026-06-12 18:24:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Apps Script 웹앱 URL - 배포 후 받은 URL로 교체하세요
 // (예: https://script.google.com/macros/s/AKfycb.../exec)
 // =====================================================
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbygk9y-R68WZeuffZop7mMkiezGgTYwtitETqPqDQa2HBzjdb0ZIlCW16igvgV7cMtD/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby4A8xEN3_Sl375dH4yUoGc1zDezsaZ1idRm52BPsRcboMbSs46IjYLOYsXVvHhBPiDAg/exec";
 
 // =====================================================
 // Firebase 설정 - 본인의 Firebase 프로젝트 설정으로 교체하세요
